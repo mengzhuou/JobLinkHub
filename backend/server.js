2179d9ea836676dd77712a0599e5cde62191e117
mengzhuou <mou30@gatech.edu>
2024-08-25 18:06:28 -0400

    created record creation and get in backend

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use('/', express.static(path.join(__dirname, '/public')));
 app.use("/", require("./routes/root"));
 app.use("/students", require("./routes/studentsRoutes"));
+app.use("/records", require("./routes/recordRoutes"));
 app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
