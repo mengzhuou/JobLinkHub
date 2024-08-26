c8c11d0d426213d30c967f047e0176d1b198c0e6
0812Sean <dachenxuan@gmail.com>
2024-08-26 15:32:33 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import EnterCode from "./components/Pages/EnterCode/EnterCode";
 import TopNavBar from "./components/Functions/TopNavBar/TopNavBar";
-
+import Application from "./components/Application/Application" 
 
 class App extends Component {
 
@@ -12,6 +12,7 @@ class App extends Component {
         <TopNavBar/>
         <Routes>
           <Route path="/" element={<EnterCode/>} />
+          <Route path="/Application" element={<Application/>}/>
         </Routes>
       </Router>
     );
